d29ae6349c7cb0d41e2037ad98b122e4a88176c0
[SW8] 박진솔 <ssol.ing99@gmail.comcat ~/.gitconfig>
2024-02-24 08:45:19 +0900

    Fix: modify 추가

---
+++ public/js/modify.js
@@ -0,0 +1,8 @@
+// 이메일, 이름, 연락처 input 태그 readOnly
+document.querySelector('.email').readOnly = true;
+document.querySelector('.name').readOnly = true;
+document.querySelector('.tel').readOnly = true;
+
+console.log(document.querySelector('.email').readOnly);
+console.log(document.querySelector('.name').readOnly);
+console.log(document.querySelector('.tel').readOnly);
