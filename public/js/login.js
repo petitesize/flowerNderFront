f89f62c84d1e333becc6cdbf37ea61263c2bbf31
[SW8] 박진솔 <ssol.ing99@gmail.comcat ~/.gitconfig>
2024-02-27 12:07:31 +0900

    Fix:파일명 수정 / Feat:로그인 POST, JWT 로컬스토리지 저장 완료

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,36 @@
+// import { API_URL } from '/public/js/constants.js'
+
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 const loginButton = document.querySelector('.login-button');
 
 loginButton.addEventListener('click', e => {
     e.preventDefault();
-    console.log('email: ', email.value);
-    console.log('password: ', password.value);
 
-    // 유저 정보가 일치하지 않을 경우 '잘못된 아이디 또는 비밀번호입니다.' alert
+    const emailVal = email.value;
+    const passwordVal = password.value;
+
+    // 이메일 입력값이 없을 경우
+    if (!emailVal) alert('아이디를 입력하세요.');
+
+    // 비밀번호 입력값이 없을 경우
+    else if (!passwordVal) alert('비밀번호를 입력하세요.');
+
+    // 로그인 정보 POST
+    fetch('http://localhost:8081/api/v1/auth/login', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: emailVal,
+            password: passwordVal,
+        }),
+    }).then(res => res.json())
+    .then(res => {
+        if (res.error) alert('잘못된 아이디 또는 비밀번호입니다.');
+        else {
+            localStorage.setItem("jwt", res.data);
+            location.href = '/index.html';
+        }
+    })
+    .catch(err => console.log(err))
 })
