3935d5d77a827bfc6fa4b07809ff4f56687714ec
[SW8] 박진솔 <ssol.ing99@gmail.comcat ~/.gitconfig>
2024-02-27 21:50:47 +0900

    sign up

--- public/js/signup.js
+++ public/js/signup.js
@@ -18,39 +18,35 @@ const signUpButton = document.querySelector('.signup-button');
 signUpButton.addEventListener('click', e => {
     e.preventDefault();
 
-    const emailVal = email.value;
-    const passwordVal = password.value;
-    const passwordConfirmVal = passwordConfirm.value;
-    const userNameVal = userName.value;
-    const phoneNumberVal = phoneNumber.value;
-    const addressVal = address.value;
-    const addressDetailVal = addressDetail.value;
+    let emailVal = email.value;
+    let passwordVal = password.value;
+    let passwordConfirmVal = passwordConfirm.value;
+    let userNameVal = userName.value;
+    let phoneNumberVal = phoneNumber.value;
+    let addressVal = address.value;
+    let addressDetailVal = addressDetail.value;
 
     const emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
-    const passwordPattern = '';
-    const phoneNumberPattern = '';
+    // const passwordPattern = '';
+    // const phoneNumberPattern = '';
 
     // 이메일 유효성 검사
     if (!emailVal || !emailPattern.test(emailVal)) {
-        console.log(emailPattern.test(emailVal));
-        console.log(email.value);
+        console.log('email 유효성 검사', emailPattern.test(emailVal));
         if (!emailBox.classList.contains('on')) {
             emailBox.classList.add('on');
-            passwordBox.style = 'border-top: 0px';
-            emailBox.style = 'height: 56px';
-            emailBox.innerHTML += '<div><p class="check-font">이메일을 정확히 입력하세요</p></div>';
-            console.log('on ---');
+            emailBox.innerHTML += '<div class="check-font"><p>이메일을 정확히 입력하세요</p></div>';
+            return false;
         }
-        
     }
 
     // 비밀번호 유효성 검사
-    else if (!passwordVal) {
+    if (!passwordVal) {
+        emailBox.removeChild(emailBox.lastChild);
+        emailBox.classList.remove('on');
         if (!passwordBox.classList.contains('on')) {
             passwordBox.classList.add('on');
-            passwordConfirmBox.style = 'border-top: 0px';
-            passwordBox.style = 'height: 56px';
-            passwordBox.innerHTML += '<div><p class="check-font">비밀번호를 입력하세요.</p></div>';
+            passwordBox.innerHTML += '<div class="check-font"><p>비밀번호를 입력하세요.</p></div>';
         }
     }
     else if (!passwordPattern.test(passwordVal)) {
@@ -77,11 +73,32 @@ signUpButton.addEventListener('click', e => {
     else if (!addressDetailVal) {
 
     }
+    else {
+        fetch('http://localhost:8081/api/v1/auth/signup', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: emailVal,
+                password: passwordVal,
+                user_name: userNameVal,
+                phone_number: phoneNumberVal,
+                address: addressVal,
+                address_detail: addressDetailVal
+            }),
+        }).then(res => res.json())
+            .then(res => {
+                if (res.error) alert('이미 가입된 이메일입니다.');
+                else {
+                    alert('가입이 완료되었습니다. 로그인해주세요.')
+                    location.href = '/uesr/login.html';
+                }
+            })
+            .catch(err => console.log(err))
+    }
 
 
     // 비밀번호 형식 검사 (최소 8글자 이상, 숫자 포함 필수)
 
-    // 비밀번호가 8글자 미만 or 숫자 포함하지 않을 경우 > '최소 8글자 이상, 숫자 포함 필수', border red
 
     // 비밀번호 - 비밀번호 확인 일치 검사
     // 일치하지 않을 경우 > '비밀번호가 일치하지 않습니다', border red
