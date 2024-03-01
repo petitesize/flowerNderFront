bbf9887e54c7b2c4936bfd91af21366403ddf555
petitesize <petitesize22@gmail.com>
2024-03-01 19:27:19 +0900

    Fix: API_URL 수정

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,3 +1,5 @@
 // 스크립트에 type="module" 넣어서 써야함!
 // VM 서버 올라오면 http://34.47.78.146/api/v1/
-export const API_URL = "http://localhost:8080/api/v1/";
+// export const API_URL = "http://localhost:8080/api/v1/";
+// export const API_URL = "http://kdt-sw-8-team05.elicecoding.com/";
+export const API_URL = "http://34.47.78.146/api/v1/";
