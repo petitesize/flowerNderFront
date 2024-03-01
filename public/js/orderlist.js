a4d430e64248c30812ca9f3a0dc3f2bc3deb7315
[SW8] 박진솔 <ssol.ing99@gmail.comcat ~/.gitconfig>
2024-03-01 22:26:53 +0900

    Fix:코드수정

--- public/js/orderlist.js
+++ public/js/orderlist.js
@@ -641,7 +641,6 @@ deleteBox.addEventListener('click', () => {
 
                       // 주문조회 결과 주문내역이 있을 경우
                       if (res.data) {
-                          console.log(res.data);
                           res.data.forEach(e => {
                               // '배송완료'를 제외한 주문이 있을 경우 회원탈퇴 불가
                               if (e.order_status !== '배송완료') {
